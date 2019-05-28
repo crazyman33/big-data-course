908790ab02e2e4f35a6c1f66d0e468d2f958f249
ZHANG Yuan <crazyman33@aliyun.com>
2019-05-28 16:43:54 +0800

    Add files via upload

---
+++ CNN/include/model.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+
+
+def model():
+    _IMAGE_SIZE = 32
+    _IMAGE_CHANNELS = 3
+    _NUM_CLASSES = 10
+
+    with tf.name_scope('main_params'):
+        x = tf.placeholder(tf.float32, shape=[None, _IMAGE_SIZE * _IMAGE_SIZE * _IMAGE_CHANNELS], name='Input')
+        y = tf.placeholder(tf.float32, shape=[None, _NUM_CLASSES], name='Output')
+        x_image = tf.reshape(x, [-1, _IMAGE_SIZE, _IMAGE_SIZE, _IMAGE_CHANNELS], name='images')
+
+        global_step = tf.Variable(initial_value=0, trainable=False, name='global_step')
+        learning_rate = tf.placeholder(tf.float32, shape=[], name='learning_rate')
+
+    with tf.variable_scope('conv1') as scope:
+        conv = tf.layers.conv2d(
+            inputs=x_image,
+            filters=32,
+            kernel_size=[3, 3],
+            padding='SAME',
+            activation=tf.nn.relu
+        )
+        conv = tf.layers.conv2d(
+            inputs=conv,
+            filters=64,
+            kernel_size=[3, 3],
+            padding='SAME',
+            activation=tf.nn.relu
+        )
+        pool = tf.layers.max_pooling2d(conv, pool_size=[2, 2], strides=2, padding='SAME')
+        drop = tf.layers.dropout(pool, rate=0.25, name=scope.name)
+
+    with tf.variable_scope('conv2') as scope:
+        conv = tf.layers.conv2d(
+            inputs=drop,
+            filters=128,
+            kernel_size=[3, 3],
+            padding='SAME',
+            activation=tf.nn.relu
+        )
+        pool = tf.layers.max_pooling2d(conv, pool_size=[2, 2], strides=2, padding='SAME')
+        conv = tf.layers.conv2d(
+            inputs=pool,
+            filters=128,
+            kernel_size=[2, 2],
+            padding='SAME',
+            activation=tf.nn.relu
+        )
+        pool = tf.layers.max_pooling2d(conv, pool_size=[2, 2], strides=2, padding='SAME')
+        drop = tf.layers.dropout(pool, rate=0.25, name=scope.name)
+
+    with tf.variable_scope('fully_connected') as scope:
+        flat = tf.reshape(drop, [-1, 4 * 4 * 128])
+
+        fc = tf.layers.dense(inputs=flat, units=1500, activation=tf.nn.relu)
+        drop = tf.layers.dropout(fc, rate=0.5)
+        softmax = tf.layers.dense(inputs=drop, units=_NUM_CLASSES, activation=tf.nn.softmax, name=scope.name)
+
+    y_pred_cls = tf.argmax(softmax, axis=1)
+
+    return x, y, softmax, y_pred_cls, global_step, learning_rate
+
+
+def lr(epoch):
+    learning_rate = 1e-3
+    if epoch > 80:
+        learning_rate *= 0.5e-3
+    elif epoch > 60:
+        learning_rate *= 1e-3
+    elif epoch > 40:
+        learning_rate *= 1e-2
+    elif epoch > 20:
+        learning_rate *= 1e-1
+    return learning_rate
